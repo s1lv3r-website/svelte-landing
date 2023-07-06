c6625b4c99cc91c2ed2cf1c97a45b81327bbf52a
theS1LV3R <me@s1lv3r.codes>
2023-07-06 13:49:55 +0200

    yes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,13 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:svelte/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:svelte/recommended', 'prettier'],
   plugins: ['@typescript-eslint'],
   /** @type {import('@typescript-eslint/types').ParserOptions & import('eslint').Linter.ParserOptions} */
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 2020,
+    ecmaVersion: 2021,
     extraFileExtensions: ['.svelte'],
   },
   env: {

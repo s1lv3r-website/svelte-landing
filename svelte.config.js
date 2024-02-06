11c2dfeab688f7a583296c237f3a54e7bbe4c5e7
theS1LV3R <me@s1lv3r.codes>
2024-02-07 00:51:46 +0100

    Run pnpx svelte-migrate sveltekit-2

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
 import netlifyAdapter from '@sveltejs/adapter-netlify';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
